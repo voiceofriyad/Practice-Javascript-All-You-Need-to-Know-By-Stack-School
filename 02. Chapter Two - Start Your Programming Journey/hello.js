a18db3bb509d44b37345b86b5786386b4e9a3753
voiceofriyad <Contactme.riyad@yahoo.com>
2020-08-20 01:40:52 +0600

    adding chapter two's 2 files

---
+++ 02. Chapter Two - Start Your Programming Journey/hello.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+console.log("I Love my country");
+console.log(20.36);
+console.log('20.36');
+console.log(9 + 9);
+console.log('9' + 9);
+console.log('Ans to the q no: ' + 12);
+
---
+++ 02. Chapter Two - Start Your Programming Journey/variables.js
@@ -0,0 +1,5 @@
+var name = 'Md. Faysal Alam Riyad';
+var age = 24;
+
+console.log(name + ' is a good boy');
+console.log('His age is: ' + age);
